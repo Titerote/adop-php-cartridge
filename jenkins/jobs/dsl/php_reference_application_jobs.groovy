813d5be6374358c6bbfc9e16101d482a6e3e3674
Tite <Titerote@users.noreply.github.com>
2018-12-11 07:43:59 -0500

    test

--- jenkins/jobs/dsl/php_reference_application_jobs.groovy
+++ jenkins/jobs/dsl/php_reference_application_jobs.groovy
@@ -116,6 +116,11 @@ unitTestJob.with {
     }
     publishers {
         archiveArtifacts("**/*")
+        step( $class: 'CloverPublisher', cloverReportDir: 'build/coverage', cloverReportFileName: 'build/logs/clover.xml')
+//  healthyTarget: [methodCoverage: 70, conditionalCoverage: 70, statementCoverage: 70], // optional, default is: method=70, conditional=80, statement=80
+//  unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50], // optional, default is none
+//  failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]     // optional, default is none
+        
         downstreamParameterized {
             trigger(projectFolderName + "/Reference_Application_Code_Analysis") {
                 condition("UNSTABLE_OR_BETTER")
