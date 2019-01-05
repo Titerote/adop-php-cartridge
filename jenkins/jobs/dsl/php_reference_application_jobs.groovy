ba46f919b0edd39ff3d7c67162fa9182061f84a3
Tite <Titerote@users.noreply.github.com>
2019-01-05 11:28:34 -0500

    updated git repo name
    Updated repository name, given that the cartridge now loads it on startup

--- jenkins/jobs/dsl/php_reference_application_jobs.groovy
+++ jenkins/jobs/dsl/php_reference_application_jobs.groovy
@@ -4,7 +4,7 @@ def projectFolderName = "${PROJECT_NAME}"
 
 // Variables
 def projectNameKey = projectFolderName.toLowerCase().replace("/", "-")
-def referenceAppgitRepo = "php-web"
+def referenceAppgitRepo = "roofgit"
 def regressionTestGitRepo = "adop-cartridge-java-regression-tests"
 def referenceAppGitUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/" + referenceAppgitRepo
 def regressionTestGitUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/" + regressionTestGitRepo
